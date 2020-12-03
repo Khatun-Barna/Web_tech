83bce7b63dd1907fa4e4aca98b3011725852cb71
Khatun-Barna <72688434+Khatun-Barna@users.noreply.github.com>
2020-12-03 16:08:50 +0600

    Delete accept.php

--- project/controller/accept.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include 'connection.php';
-    $id = $_GET['id'];
-    $q = "UPDATE `user` SET `pending`= 0 WHERE `id` = $id";
-    $query = mysqli_query($con,$q);
-    header('Location: ../RegistrationReview.php');
-?>
